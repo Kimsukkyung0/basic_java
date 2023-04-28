eed8262ab774d3fead6ff2687bf21f95b2de322e
Min kyung suk <129349523+Kimsukkyung0@users.noreply.github.com>
2023-04-28 18:22:12 +0900

    made a overiding equals method
    equals method can compare between reference variables

---
+++ VariableEquals.java
@@ -0,0 +1,35 @@
+package com.green.java.ch02;
+
+public class VariableEquals {
+    public static void main(String[] args) {
+        NumberBox nb1 = new NumberBox(10);
+        NumberBox nb2 = new NumberBox(10);
+
+        boolean r1 = (nb1 == nb2);
+        System.out.println("r1 : "+r1);
+        boolean r2 = nb1.equals(nb2);
+        System.out.println("r2 : " +r2);
+
+    }
+
+}
+
+class NumberBox{
+    private int num;
+
+    int getNum(){
+        return num;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (!(obj instanceof NumberBox)){//nb의 비교대상이 instance of numberbox가 ? ?
+            return false;
+        }
+        NumberBox nb = (NumberBox)obj;
+        return this.num == nb.getNum();
+        }
+
+    NumberBox(int num){
+        this.num = num;
+    }
+}
