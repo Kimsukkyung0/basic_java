9419fc41a3dadf40293b051771574b49b37522e1
Min kyung suk <129349523+Kimsukkyung0@users.noreply.github.com>
2023-05-05 16:21:48 +0900

    Update User.java
    Tried to use enhanced for syntax to simplify..approach to the value...that i want to use
    and added the score calculating method

--- Blackjack/User.java
+++ Blackjack/User.java
@@ -1,19 +1,41 @@
 package com.green.javasub.BlackJack;
 
+package BlackJackfin;
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class User {
-    private ArrayList<Card> usercards;
+	List<Card> usercards;
+	
+	User(){usercards = new ArrayList<>();	} 
+	
+	//Through the default constructer, put the value in the memberfield arraylist
 
-    public User(){
-        usercards = new ArrayList();
-    }
-    public void receiveCard(Card c){//이번에도 전역변수로 사용을 안하고 지역변수로 arraylist를 사용함 .
-        usercards.add(c);
-    }
-
-    public List<Card> openCards(){
-        return usercards;//주소값을 리턴한다!//값까지 리턴할 수 있는 거 만들어보기
-    }
-}
+	public void receiveCard(Card c) { usercards.add(c); }
+	
+	public List<Card> openCard(){
+		return usercards;
+	}
+	
+	private int getScore(List<Card> c) {
+		int score = 0;
+		for(Card cd : c) {
+            //Tried to use enhanced for syntax to simplify..approach to the value...that i want.....
+            
+			String tmp = cd.getDenomination();
+        	switch(tmp) {
+			case "A": 
+				score+=1;
+				break;
+			case "J" :
+			case "Q" :
+			case "K" : 
+				score+=10;
+				break;
+			default : 	
+				score = Integer.parseInt(tmp);
+			}
+		}
+		return score;
+	}//will try to seperate to two.... 
+	
