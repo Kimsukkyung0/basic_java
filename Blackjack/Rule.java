c1935b70e33cc25b007d80eda4dcf27b820d28af
Min kyung suk <129349523+Kimsukkyung0@users.noreply.github.com>
2023-05-05 23:27:19 +0900

    Update Rule.java
    added methods including scoremeter, winner.

--- Blackjack/Rule.java
+++ Blackjack/Rule.java
@@ -4,49 +4,53 @@ package com.green.javasub.BlackJack;
 import java.util.List;
 
 public class Rule {
+//	Gamer gamer = new Gamer();
+//	Dealer dealer = new Dealer();
 
-    public int getScore(List<Card> n) {
-        int score = 0;
-        for (Card c : n) {
-            String sv = c.getDenomination();
-            switch (sv) {
-                case "A":
-                    score+= 1;
-                case "J":
-                case "Q":
-                case "K":
-                    score+= 10;
-                default :
-                    score+= Integer.parseInt(sv);
-            }
-        }
-return score;
-    }
-//}
-//전체적인 흐름이 이해가 가지않음.... List<Card>
+	public int getScore(List<Card> usercards) {
+		int score = 0;
+		for(Card cd : usercards) { 
+			score += getDenominationToScore(cd);
+		}
+		return score;
+	} 
+	
+	private int getDenominationToScore(Card c) {
+		int score = 0;
+		String tmp = c.getDenomination();
+			switch(tmp) {
+			
+				case "A": 
+					score+=1;
+					break;
+				case "J" :
+				case "Q" :
+				case "K" : 
+					score+=10;
+					break;
+				default : 	
+					score = Integer.parseInt(tmp);
+			}
+		
+		return score;
+		}
+	
+	
+	public void showWhoisWin() {
+		int dscore = getScore(dealer.openCard());
+		int gscore = getScore(gamer.openCard());
+        
+//Q. How to use the value of dealer cards in the extended gamer and dealer not through the parameters or parameters???    
+//For now above two sentences are causing errorrrrrrrrrr
 
-//
-//    public int getScore(List<Card> cardList) {
-//        int sum = 0;
-//        for (Card c : cardList) {
-//            sum += getDenominationToNum(c);
-//
-//        }
-//        return sum;
-//    }
-//
-//
-//    private int getDenominationToNum(Card c){
-//        switch(c.getDenomination()){
-//            case"A": return 1;
-//            case"J":
-//            case"Q":
-//            case"K":
-//                return 10;
-//            default :
-//                return Integer.parseInt(c.getDenomination());
-//        }
-//    }
+		String winner = null;
+		if (dscore>21 && gscore >21) {System.out.println("DRAW");}
+		else if (dscore<21 && (21 - dscore> 21 - gscore)) {winner = "Gamer";}
+		else if (gscore<21 && (21 - dscore< 21 - gscore)) {winner = "Dealer";}
+		
+		System.out.printf("%s is win!",winner);
+		
+	}
+
+	
 }
-
-
