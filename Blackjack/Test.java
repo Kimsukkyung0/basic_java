7f39cf38fbdadf0be05b19a934744e4bda0ed659
Min kyung suk <129349523+Kimsukkyung0@users.noreply.github.com>
2023-05-05 23:38:18 +0900

    Update Test.java

--- Blackjack/Test.java
+++ Blackjack/Test.java
@@ -6,31 +6,48 @@ public class BlackjackTest {
     public static void main(String[] args) {
         CardDeck cd = new CardDeck();
         Gamer gamer = new Gamer();
-        Dealer dealer = new Dealer();//user인터페이스를 상속받은
+        Dealer dealer = new Dealer();
         Rule rule = new Rule();
 
-
-
-        cd.showSize();
-        Card c = cd.getCard();
-        System.out.println(c.getDenomination());
-        cd.showSize();
-
-        gamer.receiveCard(cd.getCard());
-        gamer.receiveCard(cd.getCard());
-
-        dealer.receiveCard(cd.getCard());//==>둘사이 차이점!??????????????
-        dealer.receiveCard(cd.getCard());//==>둘사이 차이점!??????????????
-
-//        List<Card> cardList = gamer.openCards();
-//        int score = rule.getScore(cardList);
-//        //=
-
-        int gamerScore = rule.getScore(gamer.openCards());
-        int dealerScore = rule.getScore(dealer.openCards());
-        System.out.println(gamerScore);
-        System.out.println(dealerScore);
-
-
-    }
-}
+        
+        System.out.println ("Game started");
+		
+		for(int i=0; i<2; i++) {
+			gamer.receiveCard(cd.getCard());
+			dealer.receiveCard(cd.getCard());
+		}//Distribution! 2cards for each
+		
+		if (rule.getScore(dealer.openCard()) < 18) {
+			dealer.receiveCard(cd.getCard());
+		}//if dealer has cards under 18, get another one automatically.
+	
+			
+		dealer.showCard(dealer.openCard());
+		gamer.showCard(gamer.openCard());
+		
+		//showcard has moved from Rule class to user. 
+        //kinda struggled cause i skipped to use opencard method when i call the showcard method! method in method..method..
+        
+        
+		Loop:
+		while(true) {
+			System.out.println("More Card? > (y/n)");
+			String answer = scanner.nextLine();		
+            if(answer.trim().equalsIgnoreCase("y")) {
+				gamer.receiveCard(cd.getCard());
+				gamer.showCard(gamer.openCard());
+		
+				continue Loop;
+			}
+			else if(answer.trim().equalsIgnoreCase("n")) {
+				
+				break;
+			}
+			else {System.out.println("Please provide the valid value");}
+				continue Loop;
+			}
+	
+		rule.showWhoisWin();
+        
+        //ERROR.
+		}
